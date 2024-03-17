366f9f13ee3365ecc18c2467653d7ef0a2a15007
bipinsinhrathod <bipinrajput548@gmail.com>
2024-03-17 21:11:39 +0530

    first commit

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:bipinrajput548@gmail.com">bipinrajput548@gmail.com</a>
+         <a href="mailto:maitri0607@gmail.com">maitri0607@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>Opening hours</h3>
+         <p>09:00am to <br> 10:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>Our address</h3>
+         <a href="#">Damru circle , Ghatlodiya , Ahmedabad , 380061 </a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>Our number</h3>
+         <a href="tel:7285081412">7285081412</a>
+         <a href="tel:8128648555">8128648555</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; Copyright @ <?= date('Y'); ?> By <span>Mr.Bipinsingh Rathore</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
